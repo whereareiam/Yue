403aef6ee4fb820cafbb5e27234ad95244b44320
whereareiam <mmm.g032@gmail.com>
2024-02-29 14:24:50 +0100

    fix: javadoc generation

--- api/src/main/java/me/whereareiam/yue/api/YuePlugin.java
+++ api/src/main/java/me/whereareiam/yue/api/YuePlugin.java
@@ -4,6 +4,13 @@ import org.pf4j.PluginWrapper;
 import org.pf4j.spring.SpringPlugin;
 import org.springframework.context.ApplicationContext;
 
+/*
+ * YuePlugin is the base class for all plugins. It provides methods for loading, enabling, disabling, and unloading the plugin.
+ *
+ * The createApplicationContext method is used to create a new ApplicationContext for the plugin. This method is called before the onLoad method.
+ *
+ * @since 0.0.1
+ */
 public abstract class YuePlugin extends SpringPlugin {
 	public YuePlugin(PluginWrapper wrapper) {
 		super(wrapper);
